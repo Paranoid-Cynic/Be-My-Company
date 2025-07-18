2eaac59951bda4cd0e432286fe0a593d02e9120f
Kaviya <kaaviya1031@gmail.com>
2025-07-18 21:58:32 +0530

    added Explore People Nearby button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-
-const HomePage = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+export default function HomePag() {
   return (
     <div>
-        <h1>Welcome to Be My Company ❤️</h1>
-        <p>Book people. have fun. spend time</p>
-        <p>Explore People nearby</p>
-        <p>start hanging out</p>
+      <h1>Welcome to Be My Company ❤️</h1>
+      <p>Book people. have fun. spend time</p>
+      <p>Explore People nearby</p>
+      <p>start hanging out</p>
+      <Link to="/ProfileList">
+        <button>Explore People Nearby</button>
+      </Link>
     </div>
-  )
+  );
 }
-
-export default HomePage
