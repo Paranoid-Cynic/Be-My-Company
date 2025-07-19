ba9ff115cd204928029e94e5ac16feabbc001dea
Kaviya <kaaviya1031@gmail.com>
2025-07-19 14:26:23 +0530

    added MyProfile page content

---
+++ src/pages/MyProfile.jsx
@@ -0,0 +1,10 @@
+export default function MyProfile() {
+  return (
+    <div>
+      <h2>My Profile</h2>
+      <p>Name: Kaaviya</p>
+      <p>Hobbies: Reading, stargazing</p>
+      <button>Edit Profile</button>
+    </div>
+  );
+}
